4d92fb4c9d122c5cb2a04d20b585e7b56b9679b6
joansenent <joansenent@hotmail.com>
2015-09-09 00:23:18 +0100

    login logaout done, step 2 in authorization challenge in progress

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :require_login
+
   def new
   	@user = User.new
   end
@@ -8,6 +10,7 @@ class UsersController < ApplicationController
   	@user = User.new(user_params)
 
   	if(@user.save)
+  		log_in(@user)
   		flash[:success] = "Welcome to Merchants!"
   		redirect_to user_path(@user)
   	else
@@ -24,6 +27,15 @@ class UsersController < ApplicationController
 
   def user_params
   	params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  def require_login
+  	unless logged_in?
+  		flash[:error] = "You must be logged in to access this section"
+  		redirect_to login_url
+  	else
+  		# redirect_to user_path(???)
+  	end
   end
 
 end
