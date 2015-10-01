033532798841d5ddb10eae0d020e0662f8f87f5a
joansenent <joansenent@hotmail.com>
2015-10-01 21:52:16 +0100

    styling header and email validator on user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,14 @@
-require_dependency 'app/validators/email_validator.rb'
+# require_dependency 'app/validators/email_validator.rb'
+class EmailValidator < ActiveModel::EachValidator
+
+	def validate_each(record, attribute, value)
+		unless value =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+      	record.errors[attribute] << (options[:message] || "is not an email")
+		end
+	end
+
+end
+
 class User < ActiveRecord::Base
 	has_many :orders, dependent: :destroy
 	before_save :downcase_email
