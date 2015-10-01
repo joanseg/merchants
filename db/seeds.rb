387c08df3eecf235aa1f6a1c4d1b96611350116f
joansenent <joansenent@hotmail.com>
2015-10-02 00:06:11 +0100

    styling in progress

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# m = Merchant.create([ { name: string }, { body: text }, { avgprice: decimal }, { image_name: string }]) 
