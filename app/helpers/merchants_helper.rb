6a62af50ef6b59aa71f9afe9376ab59644ebaaf0
joansenent <joansenent@hotmail.com>
2015-10-04 11:46:13 +0100

    add kick to seeds

--- app/helpers/merchants_helper.rb
+++ app/helpers/merchants_helper.rb
@@ -1,7 +1,7 @@
 module MerchantsHelper
 	def format_body(merchant)
 		if merchant.body && merchant.body.length != 0
-			truncate(merchant.body, length: 30, separator: '')
+			truncate(merchant.body, length: 90, separator: '')
 		else
 			content_tag(:i, 'no data')
 		end
